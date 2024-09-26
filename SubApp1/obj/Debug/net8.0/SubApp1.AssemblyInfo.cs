fbbdbf3bd5e99fe56eb11f9c205018f9ecf40273
Somayeh0503 <111448936+Somayeh0503@users.noreply.github.com>
2024-09-26 23:59:23 +0200

    Endring1

--- SubApp1/obj/Debug/net8.0/SubApp1.AssemblyInfo.cs
+++ SubApp1/obj/Debug/net8.0/SubApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbaa3e216a50d03820f447c345899689194127a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded5a13fd0aa33eab4f728445a8688084f8b6080")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
