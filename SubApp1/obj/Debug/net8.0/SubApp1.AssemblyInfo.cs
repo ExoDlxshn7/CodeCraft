1d55f17eebfb516daf2232e6837968fa54502815
Somayeh0503 <111448936+Somayeh0503@users.noreply.github.com>
2024-09-29 23:06:24 +0200

    Debuging

--- SubApp1/obj/Debug/net8.0/SubApp1.AssemblyInfo.cs
+++ SubApp1/obj/Debug/net8.0/SubApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbbdbf3bd5e99fe56eb11f9c205018f9ecf40273")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ec105f957a1a3e9c3b1ce7b87d95693a504c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
