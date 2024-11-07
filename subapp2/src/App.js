76786afeff611d9d754217383cbbcc0b8d89051c
Kristian Le Do S374967 <Therealejindo@gmail.com>
2024-11-07 22:45:39 +0100

    123
    123

--- subapp2/src/App.js
+++ subapp2/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import Index from './home/Index';
+import Profil from './home/Profil';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/home">Profil</a></li>
+    </ul>
+</nav>
+
+<Router>
+<Routes>
+  <Route path="/" element={<Index />} />
+</Routes>
+</Router>
+</div>
   );
 }
+
+
 
 export default App;
